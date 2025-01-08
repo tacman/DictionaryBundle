eb13997362674683ae5e16dd3a7170a11eb1c1f7
Tac Tacelosky <tacman@gmail.com>
2025-01-08 16:06:36 -0500

    implicit nullable

--- rector.php
+++ rector.php
@@ -5,10 +5,21 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/spec',
-        __DIR__ . '/src',
-    ])
+//    ->withPaths([
+//        __DIR__ . '/assets',
+//        __DIR__ . '/config',
+//        __DIR__ . '/demo',
+//        __DIR__ . '/packages',
+//        __DIR__ . '/public',
+//        __DIR__ . '/src',
+//    ])
     // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0);
+    ->withPhpSets(php83: true)
+    ->withPaths([__DIR__ . '/src'])
+//    ->withPhpVersion(Rector\ValueObject\PhpVersion::PHP_84)
+    ->withRules([
+        Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector::class,
+    ])
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
